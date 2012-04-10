63ea264593b2b62ac64da741c326c65d0cb8a485
Jon Maddox <jon@jonmaddox.com>
2012-04-10 16:01:02 -0400

    duh, rename these too

--- Play iOS/PLAIOSAppDelegate.m
+++ Play iOS/PLAIOSAppDelegate.m
@@ -27,9 +27,9 @@
   self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
   if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-      self.viewController = [[[PLAPlayerViewController alloc] initWithNibName:@"PLPlayerViewController_iPhone" bundle:nil] autorelease];
+      self.viewController = [[[PLAPlayerViewController alloc] initWithNibName:@"PLAPlayerViewController_iPhone" bundle:nil] autorelease];
   } else {
-      self.viewController = [[[PLAPlayerViewController alloc] initWithNibName:@"PLPlayerViewController_iPad" bundle:nil] autorelease];
+      self.viewController = [[[PLAPlayerViewController alloc] initWithNibName:@"PLAPlayerViewController_iPad" bundle:nil] autorelease];
   }
   self.window.rootViewController = self.viewController;
   [self.window makeKeyAndVisible];
