105dc006c00b0b8a2260c0eed23a9bd5bd317311
Danny Greg <danny@github.com>
2012-04-11 15:08:20 +0100

    Initial sketch of a queue window.

---
+++ Play Item/Classes/Controllers/PLAQueueWindowController.h
@@ -0,0 +1,13 @@
+//
+//  PLAQueueWindowController.h
+//  Play Item
+//
+//  Created by Danny Greg on 11/04/2012.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PLAQueueWindowController : NSWindowController
+
+@end
---
+++ Play Item/Classes/Controllers/PLAQueueWindowController.m
@@ -0,0 +1,34 @@
+//
+//  PLAQueueWindowController.m
+//  Play Item
+//
+//  Created by Danny Greg on 11/04/2012.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import "PLAQueueWindowController.h"
+
+@interface PLAQueueWindowController ()
+
+@end
+
+@implementation PLAQueueWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
