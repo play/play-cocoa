efc9fd72dda6c936215cf714cb8a2e9dc65323b0
Danny Greg <danny@github.com>
2012-04-13 22:01:40 +0100

    Draw some borders on the header background view for moar pretty.

--- Play Item/Classes/PLAHeaderBarBackgroundView.m
+++ Play Item/Classes/PLAHeaderBarBackgroundView.m
@@ -20,6 +20,15 @@
 	}
 	
 	[backingGradient drawInRect:self.bounds angle:90.0];
+	
+	NSColor *lowlightColor = [NSColor colorWithCalibratedWhite:0.0 alpha:0.32];
+	[lowlightColor set];
+	[NSBezierPath strokeLineFromPoint:NSMakePoint(NSMinX(self.bounds), NSMinY(self.bounds)) toPoint:NSMakePoint(NSMaxX(self.bounds), NSMinY(self.bounds))];
+	
+	NSColor *highlightColor = [NSColor colorWithCalibratedWhite:1.0 alpha:0.4];
+	[highlightColor set];
+	CGFloat y = NSMaxY(self.bounds) - 0.5;
+	[NSBezierPath strokeLineFromPoint:NSMakePoint(NSMinX(self.bounds), y) toPoint:NSMakePoint(NSMaxX(self.bounds), y)];
 }
 
 @end
