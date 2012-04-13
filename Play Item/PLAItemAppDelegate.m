a2e77b9c5c4bd89f4f53b660b8383a0bceedd2cc
Danny Greg <danny@github.com>
2012-04-13 12:01:40 +0100

    The appdelegate is no longer interested in a change of playing song.

--- Play Item/PLAItemAppDelegate.m
+++ Play Item/PLAItemAppDelegate.m
@@ -87,9 +87,6 @@
 }
 
 - (void)didLogIn{
-  // listen for notifications for updated songs from the CFController and pusher
-  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateWithTrackInformation) name:PLANowPlayingUpdated object:nil];
-  
   [PLATrack currentTrackWithBlock:^(PLATrack *track, NSError *err) {
     [[PLAController sharedController] setCurrentlyPlayingTrack:track];
   }];
