7c8af1937c16af4739ae2c86bd816d602d1db4a6
Danny Greg <danny@github.com>
2012-04-17 11:59:53 +0100

    Add in a value transformer from seconds to hours:minutes etc.

---
+++ vendor/XFloatSecondsToHMSStringTransformer/XFloatSecondsToHMSStringTransformer.h
@@ -0,0 +1,14 @@
+//
+//  XFloatSecondsToHMSStringTransformer.h
+//  Play Item
+//
+//  Created by Danny Greg on 17/04/2012.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+// I got lazy: http://www.cocoalab.com/?q=node/31
+
+#import <Foundation/Foundation.h>
+
+@interface XFloatSecondsToHMSStringTransformer : NSValueTransformer
+
+@end
---
+++ vendor/XFloatSecondsToHMSStringTransformer/XFloatSecondsToHMSStringTransformer.m
@@ -0,0 +1,52 @@
+//
+//  XFloatSecondsToHMSStringTransformer.m
+//  Play Item
+//
+//  Created by Danny Greg on 17/04/2012.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import "XFloatSecondsToHMSStringTransformer.h"
+
+@implementation XFloatSecondsToHMSStringTransformer
+
++ (Class)transformedValueClass
+{
+	return [NSString class];
+}
+
++ (BOOL)allowsReverseTransformation
+{
+	return YES;
+}
+
+- (id)transformedValue:(id)value {
+	if(value == nil ) return nil;
+	NSString *hmsString;
+	float timeInterval = [value floatValue];
+	int hours = (int)(timeInterval / 3600.0);
+	int minutes = (int)(timeInterval / 60.0);
+	int seconds = (int)timeInterval % 60;
+	float fraction = timeInterval - floor(timeInterval);
+	int frac = (int)(fraction * 1000.);
+	if (hours == 0) {
+		hmsString =  [NSString stringWithFormat:@"00:%02d:%02d.%i", minutes, seconds, frac];
+	} else {
+		hmsString = [NSString stringWithFormat:@"%02d:%02d:%02d.%i", hours, minutes, seconds, frac];
+	}
+	return hmsString;
+}
+
+- (id)reverseTransformedValue:(id)value {
+	if(value == nil ) return nil;
+	NSNumber *retVal;
+	int hours = (int)[[value substringToIndex:2] intValue] * 3600;
+	int minutes = (int)[[value substringWithRange:NSMakeRange(3,2)]	 intValue] * 60;
+	int seconds = (int)[[value substringWithRange:NSMakeRange(6,2)] intValue];
+	int frac = (int)[[value substringFromIndex:9] intValue];
+	float fraction = (float)frac * 0.001;
+	retVal = [NSNumber numberWithFloat:hours + minutes + seconds + fraction];
+	return retVal;
+}
+
+@end
