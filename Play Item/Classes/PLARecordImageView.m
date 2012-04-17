ab9802b8b76b20414e7d21633ecf3f35d51e452b
Danny Greg <danny@github.com>
2012-04-17 22:15:12 +0100

    Fix the sticker location on the new record image.

--- Play Item/Classes/PLARecordImageView.m
+++ Play Item/Classes/PLARecordImageView.m
@@ -15,13 +15,14 @@ static CGFloat PLARecordImageViewSpindleDiamater = 3.0;
 
 - (void)drawRect:(NSRect)dirtyRect
 {
-#warning This image isn't 100% extracted properly. I need to ask for @caged's help on that one.
     NSImage *recordImage = [NSImage imageNamed:@"record"];
 	[recordImage drawInRect:self.bounds fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
 	
+	NSRect recordBounds = NSMakeRect(2.0, 1.0, NSWidth(self.bounds) - 3.0, NSHeight(self.bounds) - 3.0); //The bounds of the actual record are not those of the imageview…
+	
 	NSRect (^centralRectOfDiamater)(CGFloat) = ^ (CGFloat diamater)
 	{
-		return NSMakeRect(floor(NSMidX(self.bounds) - (diamater / 2.0)), floor(NSMidY(self.bounds) - (diamater / 2.0)), diamater, diamater);
+		return NSMakeRect(floor(NSMidX(recordBounds) - (diamater / 2.0)), floor(NSMidY(recordBounds) - (diamater / 2.0)), diamater, diamater);
 	};
 	
 	NSRect middleStickerRect = centralRectOfDiamater(PLARecordImageViewStickerDiamater);
