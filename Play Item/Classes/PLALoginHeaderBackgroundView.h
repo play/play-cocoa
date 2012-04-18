bb4bf0cdc035d28f0ecb93e129b3dfe99b826103
Danny Greg <danny@github.com>
2012-04-18 17:55:11 +0100

    Initial start to making the login window a bit less ugleh

---
+++ Play Item/Classes/PLALoginHeaderBackgroundView.h
@@ -0,0 +1,13 @@
+//
+//  PLALoginHeaderBackgroundView.h
+//  Play Item
+//
+//  Created by Danny Greg on 18/04/2012.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PLALoginHeaderBackgroundView : NSView
+
+@end
