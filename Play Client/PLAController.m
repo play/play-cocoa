dbf6e377db547c3910429564f5ec7729342a2c35
Jon Maddox <jon@jonmaddox.com>
2013-07-12 01:30:23 -0400

    dump pusher

--- Play Client/PLAController.m
+++ Play Client/PLAController.m
@@ -10,7 +10,6 @@
 
 #import "PLAPlayClient.h"
 #import "PLATrack.h"
-#import "PTPusherChannel.h"
 
 #if TARGET_OS_EMBEDDED
 #import "Reachability.h"
@@ -20,15 +19,11 @@ NSString *const PLANowPlayingUpdated = @"PLANowPlayingUpdated";
 
 @implementation PLAController
 
-@synthesize queuedTracks, currentlyPlayingTrack, pusherClient, updateNowPlayingPusherChannelBinding, streamUrl, pusherKey;
+@synthesize queuedTracks, currentlyPlayingTrack;
 
 - (void) dealloc{
   [queuedTracks release];
   [currentlyPlayingTrack release];
-  [pusherClient release];
-  [updateNowPlayingPusherChannelBinding release];
-  [streamUrl release];
-  [pusherKey release];
 
   [super dealloc];
 }
@@ -54,11 +49,6 @@ NSString *const PLANowPlayingUpdated = @"PLANowPlayingUpdated";
 
 - (void)logInWithBlock:(void(^)(BOOL succeeded))block{
   [[PLAPlayClient sharedClient] getPath:@"/streaming_info" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-    self.streamUrl = [responseObject objectForKey:@"stream_url"];
-    self.pusherKey = [responseObject objectForKey:@"pusher_key"];
-
-		[self setUpPusher];
-    [self subscribeToChannels];
 
 	if (block != nil)
 	  block(YES);
@@ -70,42 +60,6 @@ NSString *const PLANowPlayingUpdated = @"PLANowPlayingUpdated";
 
 }
 
-#pragma mark - Pusher Bootstrap
-
-- (PTPusherChannel *)nowPlayingPusherChannel{
-  if (pusherClient) {
-    return [pusherClient subscribeToChannelNamed:@"now_playing_updates"];
-  }
-  
-  return nil;
-}
-
-- (void)setUpPusher{
-  if (pusherClient) {
-    NSLog(@"destroying pusherClient to be ready to create a new one");
-    PTPusherChannel *channel = [self nowPlayingPusherChannel];
-    [channel removeBinding:updateNowPlayingPusherChannelBinding];
-    self.updateNowPlayingPusherChannelBinding = nil;
-    
-    [self.pusherClient setDelegate:nil];
-    [self.pusherClient unsubscribeFromChannel:channel];
-    [self.pusherClient setReconnectAutomatically:NO];
-    [self.pusherClient disconnect];
-	  [self.pusherClient retain]; //intentional leak. For some when these object die they occasionally wreak havoc with a hard-as-shit crash.
-	  self.pusherClient = nil;
-  }
-
-  self.pusherClient = [PTPusher pusherWithKey:pusherKey delegate:self encrypted:NO];
-  [pusherClient setReconnectAutomatically:YES];
-  [pusherClient setReconnectDelay:30];  
-}
-
-- (void)subscribeToChannels{
-  if (pusherClient) {
-    NSLog(@"subscribing to channels");
-    PTPusherChannel *channel = [self nowPlayingPusherChannel];
-    self.updateNowPlayingPusherChannelBinding = [channel bindToEventNamed:@"update_now_playing" target:self action:@selector(channelEventPushed:)];
-  }
 }
 
 #pragma mark - Settings
@@ -146,72 +100,9 @@ NSString *const PLANowPlayingUpdated = @"PLANowPlayingUpdated";
   [[NSNotificationCenter defaultCenter] postNotificationName:PLANowPlayingUpdated object:nil];
 }
 
-#pragma mark - Channel Event handler
-
-- (void)channelEventPushed:(PTPusherEvent *)channelEvent{
-  if ([[channelEvent name] isEqualToString:@"update_now_playing"]) {
-    [self updateNowPlaying:(NSDictionary *)[channelEvent data]];
   }
-}
-
-#pragma mark - PTPusher Delegate Methods
-
-- (void)pusher:(PTPusher *)client connectionDidConnect:(PTPusherConnection *)connection{
-  NSLog(@"connectionDidConnect");
-  client.reconnectAutomatically = YES;
-}
-
-- (void)pusher:(PTPusher *)pusher didSubscribeToChannel:(PTPusherChannel *)channel {
-  NSLog(@"did subscribe to channel: %@", [channel name]);
-}
-
-- (void)pusher:(PTPusher *)pusher didFailToSubscribeToChannel:(PTPusherChannel *)channel withError:(NSError *)error{
-  NSLog(@"failed to subscribe: %@", error.description);
-}
-
-- (void)pusher:(PTPusher *)pusher didReceiveErrorEvent:(PTPusherErrorEvent *)errorEvent{
-  NSLog(@"received error event: %@", errorEvent);
-}
-
-- (void)pusher:(PTPusher *)client connectionDidDisconnect:(PTPusherConnection *)connection{
-  NSLog(@"connectionDidDisconnect");
-  
-#if TARGET_OS_EMBEDDED
-  Reachability *reachability = [Reachability reachabilityForInternetConnection];
-  
-  if ([reachability currentReachabilityStatus] == NotReachable) {
-    NSLog(@"NotReachable");
-    client.reconnectAutomatically = NO;
     
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reachabilityChanged:) name:kReachabilityChangedNotification object:reachability];
-    
-    [reachability startNotifier];
-  }else{
-    [PLATrack currentTrackWithBlock:^(PLATrack *track, NSError *error) {
-      dispatch_async(dispatch_get_main_queue(), ^(void) {
-        self.currentlyPlayingTrack = track;
-        [[NSNotificationCenter defaultCenter] postNotificationName:PLANowPlayingUpdated object:nil];
-        [self setUpPusher];
-        [self subscribeToChannels];
-      });
-    }];
-  }
   
-#else
-  [PLATrack currentTrackWithBlock:^(PLATrack *track, NSError *err) {
-    dispatch_async(dispatch_get_main_queue(), ^(void) {
-      self.currentlyPlayingTrack = track;
-      [[NSNotificationCenter defaultCenter] postNotificationName:PLANowPlayingUpdated object:nil];
-      [self setUpPusher];
-      [self subscribeToChannels];
-    });
-  }];
-	
-	[PLATrack currentQueueWithBlock:^(NSArray *tracks, NSError *err) {
-		if (tracks != nil)
-			self.queuedTracks = tracks;
-	}];
-#endif
 }
 
 #if TARGET_OS_EMBEDDED
@@ -226,8 +117,6 @@ NSString *const PLANowPlayingUpdated = @"PLANowPlayingUpdated";
     [PLATrack currentTrackWithBlock:^(PLATrack *track, NSError *error) {
       self.currentlyPlayingTrack = track;
       [[NSNotificationCenter defaultCenter] postNotificationName:PLANowPlayingUpdated object:nil];
-      [self setUpPusher];
-      [self subscribeToChannels];
     }];
 
   }
