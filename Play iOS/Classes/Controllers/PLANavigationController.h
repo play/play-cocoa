42e3b914e449addc78fd07f0cc6b514fb3bd036b
Jon Maddox <jon@jonmaddox.com>
2013-09-23 10:22:49 -0400

    add some things to the project
    * PLANavigationController - so we can control style etc
    * AirPlay icons - so we can look pretty
    * volume knob images - So we can not look silly
    * MarqueeLabel - so we can scroll long arist/album titles
    * list icon - so we can look native

---
+++ Play iOS/Classes/Controllers/PLANavigationController.h
@@ -0,0 +1,13 @@
+//
+//  PLANavigationController.h
+//  Play Cocoa
+//
+//  Created by Jon Maddox on 9/19/13.
+//  Copyright (c) 2013 GitHub, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLANavigationController : UINavigationController
+
+@end
---
+++ Play iOS/Classes/Controllers/PLANavigationController.m
@@ -0,0 +1,25 @@
+//
+//  PLANavigationController.m
+//  Play Cocoa
+//
+//  Created by Jon Maddox on 9/19/13.
+//  Copyright (c) 2013 GitHub, Inc. All rights reserved.
+//
+
+#import "PLANavigationController.h"
+
+@interface PLANavigationController ()
+
+@end
+
+@implementation PLANavigationController
+
+- (void)viewDidLoad{
+  [super viewDidLoad];
+  
+//  [self.navigationBar setBarTintColor:[UIColor colorWithRed:33.0/255.0 green:33.0/255.0 blue:33.0/255.0 alpha:1.0]];
+  [self.navigationBar setTintColor:[UIColor colorWithRed:255.0/255.0 green:0 blue:255.0/255.0 alpha:1.0]];
+}
+
+
+@end
