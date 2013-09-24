f18abc5c304ed1271877b7309377e26ff75647fb
Jon Maddox <jon@jonmaddox.com>
2013-09-23 21:49:59 -0400

    channel cell

---
+++ Play iOS/Classes/Views/PLAChannelCell.h
@@ -0,0 +1,13 @@
+//
+//  PLAChannelCell.h
+//  Play Cocoa
+//
+//  Created by Jon Maddox on 9/23/13.
+//  Copyright (c) 2013 GitHub, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLAChannelCell : UITableViewCell
+
+@end
---
+++ Play iOS/Classes/Views/PLAChannelCell.m
@@ -0,0 +1,20 @@
+//
+//  PLAChannelCell.m
+//  Play Cocoa
+//
+//  Created by Jon Maddox on 9/23/13.
+//  Copyright (c) 2013 GitHub, Inc. All rights reserved.
+//
+
+#import "PLAChannelCell.h"
+
+@implementation PLAChannelCell
+
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+  self.imageView.frame = CGRectMake(0, 0, 44.0, 44.0);
+}
+
+
+@end
